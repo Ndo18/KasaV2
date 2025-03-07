b09905e8a579c11e2d79ceb881de233d0003cc6e
Samuel Ndo <samuel.dmb@outlook.fr>
2025-03-07 14:51:20 +0100

    ✏️ 2020 to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer (){
     return (
         <footer>
             <img src={`logokasanb.png`} alt='Logo Kasa 2' className='logokasa2' />
-            <span>© 2020 Kasa. All rights reserved</span>
+            <span>© 2025 Kasa. All rights reserved</span>
         </footer>
     )
 }
